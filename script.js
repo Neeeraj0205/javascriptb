be4145eb51ba8e8900cbcfd33ba6641d47aa1bc0
neeraj <neeeraj2599@gmail.com>
2019-01-30 20:52:06 +0530

    javascript basics

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+
+let a=10;
+/*let result=" ";
+for(let i=0;i<a;i++){
+    result += i+ " ";
+}
+console.log(result);*/
+/*for(let i=0;i<a;i++){
+    console.log(i);
+}*/
+
