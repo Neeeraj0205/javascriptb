d4b83e833b5c861dce345af1f2ca2480c9555bd4
neeraj <neeeraj2599@gmail.com>
2019-02-10 13:51:41 +0530

    how to make object

---
+++ object.js
@@ -0,0 +1,13 @@
+let obj ={
+    a: 10,
+    b: 11,
+    c: {
+        p: 19,
+        r: {
+           q: 27
+        }
+    }
+}
+console.log(obj.c.r.q)
+delete obj.c.r.q ;
+console.log(obj.c.r.q)
